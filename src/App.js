cc342a0d79052bc1f01afbb36140d8760db4fa94
Aru Jain <aru1113.be23@chitkarauniversity.edu.in>
2026-04-23 12:24:03 +0530

    bugs fixed

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import CreateEvent from "./pages/CreateEvent";
+import BookingPage from "./pages/BookingPage";
+import Bookings from "./pages/Bookings";
+import Availability from "./pages/Availability";
+import EditEvent from "./pages/EditEvent";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Sidebar />
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/create" element={<CreateEvent />} />
+        <Route path="/book/:eventId" element={<BookingPage />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/availability" element={<Availability />} />
+        <Route path="/edit/:id" element={<EditEvent />} />
+
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/EditEvent.jsx
@@ -0,0 +1,56 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "../css/e.css";
+
+function EditEvent() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [duration, setDuration] = useState("");
+  const [slug, setSlug] = useState("");
+
+  useEffect(() => {
+   axios.get(`${process.env.REACT_APP_API_URL}/events`)
+      .then((res) => {
+        const event = res.data.find((e) => e.id === id);
+        if (event) {
+          setTitle(event.title);
+          setDuration(event.duration);
+          setSlug(event.slug);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`${process.env.REACT_APP_API_URL}/events/${id}`, {
+        title,
+        duration,
+        slug,
+      });
+
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Edit Event</h1>
+
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input value={duration} onChange={(e) => setDuration(e.target.value)} />
+        <input value={slug} onChange={(e) => setSlug(e.target.value)} />
+
+        <button onClick={handleUpdate}>Update</button>
+      </div>
+    </div>
+  );
+}
+
+export default EditEvent;
